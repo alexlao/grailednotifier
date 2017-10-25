e97a4b3a38d5c459286e33ed1a6d82210f12c5a2
Alexander Lao <lao.alex97@gmail.com>
2017-10-25 16:33:53 -0400

    Gets unique ID from every item returned from search query

--- search.py
+++ search.py
@@ -2,8 +2,8 @@ import requests
 import json
 import urllib 
 
-def buildPost():
-	encoded = urllib.parse.urlencode({'query': 'gucci flames', 'hitsPerPage':'100', 'page':'0'}, safe='/', quote_via=urllib.parse.quote)
+def postSearch(requestedItem):
+	encoded = urllib.parse.urlencode({'query': requestedItem, 'hitsPerPage':'100', 'page':'0'}, safe='/', quote_via=urllib.parse.quote)
 	filters = ['(strata%3A\'grailed\'', 'strata%3A\'hype\'', 'strata%3A\'basic\')']
 	marketplaces = ['grailed','heroine']
 	queryParameters = {'x-algolia-agent': 'Algolia for vanilla JavaScript 3.21.1', 'x-algolia-application-id': 'MNRWEFSS2Q', 'x-algolia-api-key':'a3a4de2e05d9e9b463911705fb6323ad'}
@@ -11,25 +11,20 @@ def buildPost():
 
 	payload = {'params':encoded+'&filters='+filtersConcat}
 	stringLoad = json.dumps(payload)
-	print(json.dumps(payload))
+	# print(json.dumps(payload))
 
 	#each item post has its unique id 
-	
-	# print(filtersConcat)
-
-	#filters = urllib.parse.urlencode({'filters':'(strata:'grailed')'})
-	# print('filters: ' + filters)
-	# print(json.dumps(queryParameters))
 	headers = {'Content-Type':'application/x-www-form-urlencoded'}
 	request = requests.post('https://mnrwefss2q-dsn.algolia.net/1/indexes/Listing_by_date_added_production/query', params=queryParameters, headers=headers,data=stringLoad)
-	print(request.text)
-	print(request.url)
+	#print(request.text)
+	responseDict = json.loads(request.text)
+	return responseDict
+
 def main():
-	# response = requests.get('https://api.coinmarketcap.com/v1/ticker/?limit=10')
-	# jsonObj = response.json()
-	# print(jsonObj[0]['id'])
-	# print(response.text)
-	# print('done')
-	buildPost()
+	searchItem = 'gucci flames'
+	results = postSearch(searchItem)
+	for arr in results['hits']:
+		print(arr['id'])
+	print(len(results['hits']))
 if __name__ == "__main__":
 	main()
